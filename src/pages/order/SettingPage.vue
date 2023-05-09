21e3ba71274613ad557c8b7000a253b5df4c2ea0
baoshie <online988988@gmail.com>
2023-05-09 14:53:23 +0800

    333

--- src/pages/order/SettingPage.vue
+++ src/pages/order/SettingPage.vue
@@ -27,7 +27,6 @@ type="number"
 style="width: 130px;"
 v-model="bangdong1TotalQuantity"
 label="數量總計"
-:rules="[val => !!val || '此欄位為必填']"
 readonly
 />
 <q-input
@@ -37,7 +36,6 @@ type="number"
 style="width: 130px;"
 v-model="bangdong1TotalQuantityPrice"
 label="金額總計"
-:rules="[val => !!val || '此欄位為必填']"
 readonly
 />
 </div>
@@ -57,9 +55,48 @@ class="q-mt-md"
 />
 
 </div >
-<q-table>
+<q-table :rows="tableRows">
+  <template #header>
+    <q-tr>
+      <q-th style="font-size:20px">商品</q-th>
+      <q-th style="font-size:20px">數量</q-th>
+      <q-th style="font-size:20px">操作</q-th>
+    </q-tr>
+  </template>
 
+  <template #body="props">
+    <q-tr :props="props">
+      <q-td style="text-align: center;font-size:20px">{{ props.row.name }}</q-td>
+      <q-td style="text-align: center;font-size:20px">{{ props.row.quantity }}</q-td>
+      <q-td style="text-align: center">
+        <q-btn icon="delete"
+        color="red"
+        fab-mini
+        unelevated
+        size="sm"
+        @click="deleteRow(props.row.id)" />
+        <q-btn icon="edit"
+        fab-mini
+        unelevated size="sm"
+        color="info"
+        @click="editRowDialog(props.row)"/>
+      </q-td>
+    </q-tr>
+  </template>
 </q-table>
+
+<q-dialog v-model="showEditDialog">
+  <q-card>
+    <q-card-section>
+      <q-input v-model="editData.name" label="商品"></q-input>
+      <q-input v-model="editData.quantity" label="數量"></q-input>
+    </q-card-section>
+    <q-card-actions>
+      <q-btn label="取消" color="primary" @click="showEditDialog = false"></q-btn>
+      <q-btn label="保存" color="primary" @click="saveEditData"></q-btn>
+    </q-card-actions>
+  </q-card>
+</q-dialog>
 
 </template>
 
@@ -70,6 +107,7 @@ import { apiAuth } from 'src/boot/axios'
 const bnagdong1Mains = reactive([])
 const bangdongMainName = ref('')
 const price = ref(0)
+const tableRows = reactive([])
 
 const loadBangdongMain = async () => {
   try {
@@ -103,4 +141,48 @@ const bangdong1TotalQuantity = computed(() => {
 const bangdong1TotalQuantityPrice = computed(() => {
   return bangdong1TotalQuantity.value * price.value
 })
+const submitBangdong1 = () => {
+  const row = {
+    name: '',
+    quantity: ''
+  }
+
+  for (const bnagdong1Main of bnagdong1Mains) {
+    if (bnagdong1Main.quantity > 0) {
+      row.name = '招牌便當' + bangdong1TotalQuantity.value
+      row.quantity += bnagdong1Main.name + '' + parseInt(bnagdong1Main.quantity).toString() + ' '
+    }
+  }
+
+  // 將這個 row 推進 tableRows 中
+  tableRows.push(row)
+}
+
+const deleteRow = (id) => {
+  const index = tableRows.findIndex((row) => row.id === id)
+  if (index >= 0) {
+    tableRows.splice(index, 1)
+  }
+}
+const showEditDialog = ref(false)
+const editData = reactive({
+  id: null,
+  name: '',
+  quantity: 0
+})
+
+const editRowDialog = (row) => {
+  editData.name = row.name
+  editData.quantity = row.quantity
+  showEditDialog.value = true
+}
+const saveEditData = () => {
+  const index = tableRows.findIndex((row) => row.name === editData.name && row.quantity === editData.quantity)
+  if (index >= 0) {
+    tableRows[index].name = editData.name
+    tableRows[index].quantity = editData.quantity
+  }
+  showEditDialog.value = false
+}
+
 </script>
--- src/pages/order/TestInput1001.vue
+++ src/pages/order/TestInput1001.vue
@@ -1,43 +1,188 @@
 <template>
-  <q-page>
-    <q-card class="q-gutter-md row items-start  word-break " >
 
-      <div style="width:100%;padding: 0;margin: 10px;height: 30px;">
-            <h5  style="line-height: 0;">主餐</h5>
-            </div>
-      <div v-for="product in products" :key="product.id" class="row">
-        <q-input
-          v-model="product.quantity"
-          :label="product.name"
-          type="number"
-          outlined
-          :input-style="{ fontSize: '20px' }"
-          style="width: 130px;"
-        ></q-input>
-      </div>
-  </q-card>
-  </q-page>
+  <div>
+<h5 style="margin: 0; padding: 0;">{{ bangdongMainName }}</h5>
+<div class="row items-center"
+>
+<q-item  v-for="(bnagdong1Main, index) in bnagdong1Mains" :key="index" class="flex-auto " style="margin: 0; padding: 0;">
+<q-input
+v-model="bnagdong1Main.quantity" :label="bnagdong1Main.name"
+outlined
+type="number"
+  :input-style="{ fontSize: '20px', margin: '0' }"
+  :label-style="{ fontSize: '20px' }"
+  style="width: 130px;"
+  />
+</q-item>
+</div>
+</div>
+<div style="width:100%;padding: 0;margin: 0;height: 30px;">
+    <h5  style="line-height: 0;">招牌便當</h5>
+    </div>
+    <div class="row">
+<q-input
+outlined
+type="number"
+:input-style="{ fontSize: '20px' }"
+style="width: 130px;"
+v-model="bangdong1TotalQuantity"
+label="數量總計"
+:rules="[val => !!val || '此欄位為必填']"
+readonly
+/>
+<q-input
+outlined
+type="number"
+:input-style="{ fontSize: '20px' }"
+style="width: 130px;"
+v-model="bangdong1TotalQuantityPrice"
+label="金額總計"
+:rules="[val => !!val || '此欄位為必填']"
+readonly
+/>
+</div>
+<div class="row justify-end" style="width: 100%;">
+<q-btn
+color="primary"
+label="送出"
+type="submit"
+class="q-mt-md"
+@click="submitBangdong1"
+/>
+<q-btn
+color="red"
+label="取消"
+@click="showbangdong1 = false"
+class="q-mt-md"
+/>
+
+</div >
+<q-table :rows="tableRows">
+<template #header>
+<q-tr>
+<q-th>商品</q-th>
+<q-th>數量</q-th>
+<q-th></q-th>
+</q-tr>
+</template>
+
+<template #body="props">
+<q-tr :props="props">
+<q-td>{{ props.row.name }}</q-td>
+<q-td>{{ props.row.quantity }}</q-td>
+<q-td>
+<q-btn icon="delete"
+color="red"
+fab-mini
+unelevated
+size="sm"
+@click="deleteRow(props.row.id)" />
+<q-btn icon="edit"
+fab-mini
+unelevated size="sm"
+color="info"
+@click="editRowDialog(props.row)"/>
+</q-td>
+</q-tr>
+</template>
+</q-table>
+
+<q-dialog v-model="showEditDialog">
+<q-card>
+<q-card-section>
+<q-input v-model="editRowData.quantity" label="數量" type="number" />
+</q-card-section>
+<q-card-actions align="right">
+<q-btn label="Cancel" color="negative" @click="showEditDialog = false" />
+<q-btn label="Save" color="primary" @click="saveEditRow" />
+</q-card-actions>
+</q-card>
+</q-dialog>
+
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 import { apiAuth } from 'src/boot/axios'
 
-const products = ref([])
+const bnagdong1Mains = reactive([])
+const bangdongMainName = ref('')
+const price = ref(0)
+const tableRows = reactive([])
 
-const fetchProducts = async () => {
+const loadBangdongMain = async () => {
   try {
     const response = await apiAuth.get('catalog/product/1001')
-    products.value = response.data.product_options.main_mealproduct_option_values[0].map(product => ({
-      id: product.id,
-      name: product.short_name,
-      quantity: 0
-    }))
+    const productOptions = response.data.product_options
+    const mainMeal = productOptions.main_meal
+    const values = mainMeal.product_option_values
+    for (const value of values) {
+      bnagdong1Mains.push({
+        id: value.id,
+        name: value.short_name,
+        quantity: 0
+      })
+    }
+    bangdongMainName.value = mainMeal.name
+    price.value = parseInt(response.data.price)
   } catch (error) {
     console.error(error)
   }
 }
 
-fetchProducts()
+loadBangdongMain()
+// 計算總數量
+const bangdong1TotalQuantity = computed(() => {
+  return bnagdong1Mains.reduce((total, current) => {
+    return total + parseInt(current.quantity)
+  }, 0)
+})
+
+// 計算總價格
+const bangdong1TotalQuantityPrice = computed(() => {
+  return bangdong1TotalQuantity.value * price.value
+})
+const submitBangdong1 = () => {
+  for (const bnagdong1Main of bnagdong1Mains) {
+    if (bnagdong1Main.quantity > 0) {
+      const row = {
+        id: bnagdong1Main.id,
+        name: '招牌便當' + bnagdong1Main.name,
+        quantity: parseInt(bnagdong1Main.quantity).toString()
+      }
+      tableRows.push(row)
+    }
+  }
+}
+
+const deleteRow = (id) => {
+  const index = tableRows.findIndex((row) => row.id === id)
+  if (index >= 0) {
+    tableRows.splice(index, 1)
+  }
+}
+const showEditDialog = ref(false)
+const editRowData = reactive({
+  id: null,
+  name: '',
+  quantity: 0
+})
+
+const editRowDialog = (row) => {
+  editRowData.id = row.id
+  editRowData.name = row.name
+  editRowData.quantity = parseInt(row.quantity)
+  showEditDialog.value = true
+}
+const saveEditRow = () => {
+  const index = tableRows.findIndex((row) => row.id === editRowData.id)
+  if (index >= 0) {
+    tableRows[index].quantity = parseInt(editRowData.quantity).toString()
+  }
+  editRowData.id = null
+  editRowData.name = ''
+  editRowData.quantity = 0
+  showEditDialog.value = false
+}
 
 </script>
